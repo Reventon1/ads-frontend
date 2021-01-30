39f825ee3a4cf15e4701b23fa3855089d55e552c
unknown < >
2021-01-30 11:49:41 +0300

    ads-frontend

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+    var arrayAds = Array();
+
     $("#modal").hide();
 
     $("#add").click(function() {
@@ -9,4 +12,43 @@ $(document).ready(function() {
         $("#modal").hide();
     });
 
+    $('#new').click(function() {
+        var ad = {
+            text: $('textarea[name="text"]').val(),
+            contactName: $('input[name="name"]').val(),
+            contactPhone: $('input[name="phone"]').val(),
+        }
+        
+
+        arrayAds.push(ad);
+        console.log(arrayAds)
+
+        renderAds();
+
+        $("#modal").hide();
+  
+    });
+
+    // функция отрисовки новых объявлений
+    function renderAds() {
+        // очищаем блок контента
+        $('#content').html('');
+
+        //отрисовываем новые объявления
+        arrayAds.map(ad => {
+            var adHTML = 
+            '<div class="Content__box">' +
+                '<div class="Box__info">'+ ad.text +'</div>' +
+                '<div class="Box__name">'+ ad.contactName +'</div>' +
+                '<div class="Box__phone">'+ ad.contactPhone +'</div>' +
+            '</div>';
+        
+            $('#content').append(adHTML);
+        });
+
+
+
+
+
+    }
 });
